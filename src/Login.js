f41628fac618f5bae1994cf23bf84bd92fd622fc
dxa152 <dxa152@cs.bham.ac.uk>
2023-01-09 20:19:41 +0000

    console Log

--- src/Login.js
+++ src/Login.js
@@ -38,7 +38,7 @@ const Login = ({setAuth}) =>{
     }
 
     }).catch((error) => {
-        alert(error.message);
+        console.log('sign in error'. error.message);
         const errorCode = error.code;
         const errorMessage = error.message;
         const email = error.customData.email;
